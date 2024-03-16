98c9bfbc69687e4f0cd6a6cbee25f03cefb1ec49
Vinicius E Santo <viniciusesant@gmail.com>
2024-03-16 11:10:51 -0300

    Finalização de Projeto
    - Adição de novas imagens
    - conserto de bugs de funcionalidade.

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-function loader(){
-
+function loader() {
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
+    var img = window.document.getElementById('image')
     var data = new Date()
     var time = data.getHours()
     msg.innerHTML = `O horario atual é: ${time} horas`
-
     if (time >= 0 && time < 12) {
-        img.scr = 'manha.jpg'
-    } else if (time >= 12 && time < 18) {
-        img.src = 'tarde.jpg'
+        img.src = './Images/manha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (time >= 12 && time <= 18) {
+        img.src = './Images/tarde.jpg'
+        document.body.style.background = '#b9846f'
     } else {
-        img.src = 'noite.jpg'
+        img.src = './Images/noite.jpg'
+        document.body.style.background = '#515154'
     }
 }
